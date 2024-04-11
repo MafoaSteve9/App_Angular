a826e9ed5a3eaafd2076b40eaf705f6d7424eaa4
Mafoa <steve_mafoa@outlook.com>
2024-04-11 15:32:18 +0200

    Mise en place d'une méthode pour revenir sur notre route principale

---
+++ ng-pokemon-app/src/app/list-pokemon/list-pokemon.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Pokemon } from '../pokemon';
+import { POKEMONS } from '../mock-pokemon-list';
+import { BorderCardDirective } from '../border-card.directive';
+import { PokemonTypeColorPipe } from '../pokemon-type-color.pipe';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-list-pokemon',
+  standalone: true,
+  imports: [BorderCardDirective, PokemonTypeColorPipe, CommonModule],
+  templateUrl: './list-pokemon.component.html'
+})
+export class ListPokemonComponent {
+  pokemonList: Pokemon[] = POKEMONS;
+
+  constructor(private router: Router) {}
+
+  goToPokemon(pokemon: Pokemon) {
+    this.router.navigate(['/pokemon/', pokemon.id]);
+  }
+}
