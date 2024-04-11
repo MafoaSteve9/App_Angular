0015406a15338c5688483a048c8ea7f4e1ca0fd0
Mafoa <steve_mafoa@outlook.com>
2024-04-11 15:30:45 +0200

    importation du RouterOutlet pour afficher notre titre sur toute nos routes

--- ng-pokemon-app/src/app/app.component.ts
+++ ng-pokemon-app/src/app/app.component.ts
@@ -1,35 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { POKEMONS } from './mock-pokemon-list';
-import { Pokemon } from './pokemon';
-import { BorderCardDirective } from './border-card.directive';
-import { CommonModule } from '@angular/common';
-import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: 'app.component.html',
-  imports: [BorderCardDirective, CommonModule, PokemonTypeColorPipe]
+  imports: [RouterOutlet]
 })
-export class AppComponent implements OnInit {
-  pokemonList: Pokemon[] = POKEMONS;
-  pokemonSelected: Pokemon|undefined;
-
-  ngOnInit() {
-      console.table(this.pokemonList);
-  }
-
-  selectPokemon(pokemonId: number) {
-    const pokemon: Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
-
-    if (pokemon) {
-      console.log(`Vous avez demandé le pokémon ${pokemon.name}`);
-      this.pokemonSelected = pokemon;
-    } else {
-      console.log(`Vous avez demandé un pokémon qui n'existe pas.`);
-      this.pokemonSelected = pokemon;
-    }
-    
-  }
-}
+export class AppComponent {}
