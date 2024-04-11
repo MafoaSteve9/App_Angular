699cc5f6c49dd3fc9da6aab4fca80ee3774e4394
Mafoa <steve_mafoa@outlook.com>
2024-04-11 15:42:51 +0200

    On redirige l'utilisateur sur la route principale grâce à notre methode goToPokemonList

---
+++ ng-pokemon-app/src/app/detail-pokemon/detail-pokemon.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { POKEMONS } from '../mock-pokemon-list';
+import { Pokemon } from '../pokemon';
+import { CommonModule } from '@angular/common';
+import { PokemonTypeColorPipe } from '../pokemon-type-color.pipe';
+
+@Component({
+  selector: 'app-detail-pokemon',
+  standalone: true,
+  imports: [CommonModule, PokemonTypeColorPipe],
+  templateUrl: './detail-pokemon.component.html'
+})
+export class DetailPokemonComponent implements OnInit {
+
+  pokemonList: Pokemon[];
+  pokemon: Pokemon|undefined;
+
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit() {
+    this.pokemonList = POKEMONS;
+    const pokemonId: string|null = this.route.snapshot.paramMap.get('id');
+    if(pokemonId) {
+      this.pokemon = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
+    }
+  }  
+  goToPokemonList() {
+    this.router.navigate(['/pokemons']);
+  }
+}
+
